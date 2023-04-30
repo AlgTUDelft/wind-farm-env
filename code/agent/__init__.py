8fd0743b7d7ea30ab0515a2a8090dbc0687ac068
Greg Neustroev <G.Neustroev@tudelft.nl>
2023-04-30 11:17:26 +0200

    Moved the environment to upper level

--- code/agent/__init__.py
+++
@@ -1,7 +0,0 @@
-from .agent import Agent
-from .deep.sac import SACAgent
-from .deep.td3 import TD3Agent
-from .naive_agent import NaiveAgent
-from .floris_agent import FlorisAgent
-
-__all__ = ['Agent', 'NaiveAgent', 'FlorisAgent', 'SACAgent', 'TD3Agent']
--- code/agent/deep/__init__.py
+++
@@ -1,5 +0,0 @@
-from .replay_buffer import ReplayBuffer
-from .td3 import TD3Agent
-from .sac import SACAgent
-
-__all__ = ['ReplayBuffer', 'TD3Agent', 'SACAgent']
